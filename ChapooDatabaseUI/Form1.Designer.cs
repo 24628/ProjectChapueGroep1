ac06230f4fcb78bbdd54f49cee7d7397f06092ea
insertcoolname <47336104+insertcoolname@users.noreply.github.com>
2021-05-19 15:17:57 +0200

    Fixed error where the version where not correct

---
+++ ChapooDatabaseUI/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿
+namespace ChapooUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LoginFormEmailTextBox = new System.Windows.Forms.TextBox();
+            this.LoginFormPasswordTextBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // LoginFormEmailTextBox
+            // 
+            this.LoginFormEmailTextBox.Location = new System.Drawing.Point(359, 128);
+            this.LoginFormEmailTextBox.Name = "LoginFormEmailTextBox";
+            this.LoginFormEmailTextBox.Size = new System.Drawing.Size(260, 27);
+            this.LoginFormEmailTextBox.TabIndex = 0;
+            // 
+            // LoginFormPasswordTextBox
+            // 
+            this.LoginFormPasswordTextBox.Location = new System.Drawing.Point(359, 189);
+            this.LoginFormPasswordTextBox.Name = "LoginFormPasswordTextBox";
+            this.LoginFormPasswordTextBox.Size = new System.Drawing.Size(260, 27);
+            this.LoginFormPasswordTextBox.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(492, 253);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(126, 33);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Submit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.LoginFormSubmitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.LoginFormPasswordTextBox);
+            this.Controls.Add(this.LoginFormEmailTextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox LoginFormEmailTextBox;
+        private System.Windows.Forms.TextBox LoginFormPasswordTextBox;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ ChapooDatabaseUI/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace ChapooUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginFormSubmitButton_Click(object sender, EventArgs e)
+        {
+            ChapooDatabaseLogic.AuthenticationService AuthenticationService = new ChapooDatabaseLogic.AuthenticationService();
+
+            // get the email and password from textboxes
+            string email = LoginFormEmailTextBox.Text;
+            string password = LoginFormPasswordTextBox.Text;
+
+            // if validuser then show application and hide login and register and password forgotten
+            if (AuthenticationService.CheckUserLogin(email, password))
+            {
+                MessageBox.Show("Login succesful!");
+                return;
+            }
+            else
+            {
+                MessageBox.Show("Wrong email or password!"); // show messagebox with message, application keeps running
+                return;
+            }
+        }
+    }
+}
