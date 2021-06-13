b7f53312870c38e4f0a0b17929511a07363c80b5
Merlijn Busch <merlijn1busch@gmail.com>
2021-06-13 14:28:37 +0200

    added small chabge

--- ChapooDatabaseModel/MenuItem.cs
+++ ChapooDatabaseModel/MenuItem.cs
@@ -12,12 +12,6 @@ namespace ChapooDatabaseModel
 
         public MenuItem(int v1, string v2, decimal v3)
         {
-//<<<<<<< HEAD
-            //this.MenuItemID = v1;
-            //this.MenuID = v2;
-            //this.MenuName = v3;
-            //this.Price = v4;
-//=======
             this.Id = v1;
             this.Name = v2;
             this.Price = v3;
@@ -30,7 +24,6 @@ namespace ChapooDatabaseModel
                 m.Name,
                 string.Format("{0:C}", m.Price)
             };
-//>>>>>>> 837202a168db295a6b325955762b9c0b29bc8029
         }
     }
 }
